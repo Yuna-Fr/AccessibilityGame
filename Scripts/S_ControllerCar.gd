7172903582aa50d25e492663b180d8eb235d1c66
M-Lampin <maxime.lampin.41@gmail.com>
2026-01-27 16:24:16 +0100

    updating car controller with life and stun

--- Scripts/S_ControllerCar.gd
+++ Scripts/S_ControllerCar.gd
@@ -1,6 +1,10 @@
 class_name ControllerCar extends CharacterBody2D
 
 @export var speed: float = 500.0
+@export var life: int = 3
+var isdead: bool = false
+@onready var timer = $Timer
+@onready var MeshColor = $MeshInstance2D
 
 func _ready():
 	pass
@@ -8,8 +12,15 @@ func _ready():
 func _physics_process(delta):
 	var direction := 0.0
 	
-	if Input.is_action_pressed("Move_Up"): direction -= 1
-	if Input.is_action_pressed("Move_Down"): direction += 1
+	if life==0:
+		gameover()
+	
+	if isdead == true:
+		diestate(delta)
+	
+	if isdead == false:
+		if Input.is_action_pressed("Move_Up"): direction -= 1
+		if Input.is_action_pressed("Move_Down"): direction += 1
 
 	velocity.y = direction * speed
 	move_and_slide()
@@ -17,3 +28,28 @@ func _physics_process(delta):
 	# Clamp Y position to screen
 	var screen_height = get_viewport_rect().size.y
 	position.y = clamp(position.y, 0, screen_height)
+
+
+func _on_area_2d_area_entered(area: Area2D) -> void:
+	die()
+	pass # Replace with function body.
+
+func die():
+	isdead = true
+	timer.start()
+	life -= 1
+	print("life : ", life)
+	
+
+func diestate(delta):
+	#MeshColor.modulate(Color.RED)
+	MeshColor.rotate(20 * delta)
+
+func gameover():
+	queue_free()
+	print("gameover")
+	pass
+
+func _on_timer_timeout() -> void:
+	isdead = false
+	pass # Replace with function body.
