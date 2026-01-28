4bfefdba839568cde5e7df420097546486a83e49
M-Lampin <maxime.lampin.41@gmail.com>
2026-01-28 15:32:30 +0100

    change collision layer

--- Scripts/S_ControllerCar.gd
+++ Scripts/S_ControllerCar.gd
@@ -3,7 +3,7 @@ class_name ControllerCar extends CharacterBody2D
 @export var speed: float = 500.0
 @export var life: int = 3
 var isdead: bool = false
-var candie: bool = true
+var canDie: bool = true
 @onready var timer = $Timer
 @onready var MeshColor = $MeshInstance2D
 
@@ -13,7 +13,7 @@ func _ready():
 func _physics_process(delta):
 	var direction := 0.0
 	
-	if life==0 && candie:
+	if life==0 && canDie:
 		gameover()
 	
 	if isdead == true:
