63535260b18128bf8b38e1f9e0d2c87f329a6a40
M-Lampin <maxime.lampin.41@gmail.com>
2026-01-28 10:12:51 +0100

    Random texture selected when created + size modification

--- Scripts/S_Obstacle.gd
+++ Scripts/S_Obstacle.gd
@@ -3,8 +3,13 @@ extends Area2D
 @export var speed: float = 200
 var ismoving: bool = false
 
+@export var texturelist: Array[Texture]
+
+@onready var sprite = $Sprite2D
+
 func _ready():
 	ismoving = true
+	sprite.texture = texturelist.pick_random()
 	
 func _physics_process(delta):
 	move_local_x(-speed * delta)
