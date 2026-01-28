0960790468f4f4bfd751e21d8a5e9f95f976ca90
ColorMan777 <97909940+ColorMan777@users.noreply.github.com>
2026-01-28 15:43:17 +0100

    change scene packed + sons + etc jsplus ahaha

--- Scripts/Managers/S_GameManager.gd
+++ Scripts/Managers/S_GameManager.gd
@@ -4,12 +4,13 @@ static var instance : S_GameManager
 
 @export var scene : Array[PackedScene]
 
+
 var current_index : int = 0
 var current_scene : Node
 	
 func _ready() -> void:
 	instance = self
-	swapScene(0)
+	#swapScene(0)
 
 func _physics_process(delta):
 	if Input.is_action_just_released("SwapScene"):
@@ -23,8 +24,9 @@ func swapScene(scene_index: int):
 		push_warning("Scene index out of range")
 		return
 
-	if current_scene: current_scene.queue_free()
-
+	#if current_scene: current_scene.queue_free()
+#
 	current_index = scene_index
-	current_scene = scene[scene_index].instantiate()
-	add_child(current_scene)
+	#current_scene = scene[scene_index].instantiate()
+	#add_child(current_scene)
+	get_tree().change_scene_to_packed(scene[scene_index])
