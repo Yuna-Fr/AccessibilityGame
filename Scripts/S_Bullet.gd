e7d087f52c29309460e0fc88625e27e2a9870cc6
M-Lampin <maxime.lampin.41@gmail.com>
2026-01-28 14:53:13 +0100

    New bullet size and new variable for adjusting the size

--- Scripts/S_Bullet.gd
+++ Scripts/S_Bullet.gd
@@ -1,6 +1,11 @@
 extends Area2D
 
 @export var speed: float = 800
+@export var size: float = 1
+
+func _ready() -> void:
+	scale = scale * size
+	pass
 
 func _physics_process(delta):
 		position.x += speed * delta
