d389c52f3afb225aa028d07efbcdde188d70a03c
M-Lampin <maxime.lampin.41@gmail.com>
2026-01-29 10:38:02 +0100

    Adjusting spawner with HP obstacle

--- Scripts/S_Obstacle.gd
+++ Scripts/S_Obstacle.gd
@@ -1,4 +1,5 @@
 extends Area2D
+class_name Obstacle
 
 @export var speed: float = 200
 var ismoving: bool = false
@@ -7,16 +8,31 @@ var ismoving: bool = false
 
 @onready var sprite = $Sprite2D
 
+var life = 1
+
 func _ready():
 	ismoving = true
 	sprite.texture = texturelist.pick_random()
-	
+
+
+
 func _physics_process(delta):
 	move_local_x(-speed * delta)
+	
+	if(life == 0):
+		die()
+	
 
 func _on_body_entered(body: Node2D) -> void:
 	queue_free()
 	print("bouffon")
 
 func _on_area_entered(body: Area2D) -> void:
+	life -=1
+
+func die():
 	queue_free()
+
+func setHp(hp: int):
+	life = hp
+	pass
