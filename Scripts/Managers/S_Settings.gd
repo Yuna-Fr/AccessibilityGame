8ad2078e64167d1224ead965965687e1f72061da
Yuna <yuna.bonnifet@gmail.com>
2026-01-29 17:07:31 +0100

    Add  onebutton on car game

--- Scripts/Managers/S_Settings.gd
+++ Scripts/Managers/S_Settings.gd
@@ -1,14 +1,28 @@
 extends MarginContainer
 
-@export_category("Shooter")
+@export_group("Car")
+@export var c_one_button : CheckBox
+
+@export_group("Shooter")
 @export var s_one_button : CheckBox
 
-@export_category("Platformer")
+@export_group("Platformer")
 @export var p_one_button : CheckBox
 
+@export var daltonism_mode : CheckBox
+
 func _ready():
+	c_one_button.toggled.connect(c_toggle_one_button)
 	s_one_button.toggled.connect(s_toggle_one_button)
 	p_one_button.toggled.connect(p_toggle_one_button)
+	
+	daltonism_mode.toggled.connect(toggle_daltonism_mode)
+
+#region Shooter
+func c_toggle_one_button(toggled : bool): 
+	CarController.OneButton = toggled
+
+#endregion
 
 #region Shooter
 func s_toggle_one_button(toggled : bool): 
@@ -19,5 +33,12 @@ func s_toggle_one_button(toggled : bool):
 #region Platformer
 func p_toggle_one_button(toggled : bool): 
 	PlatformerController.OneButton = toggled
+
+#endregion
+
+#region Other
+func toggle_daltonism_mode(toggled : bool):
+	#sget_instance_shader_parameter()
+	pass
 
 #endregion
