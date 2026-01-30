a990d1c6fa29a5e5f2e47df8a28fb10af0248e27
ColorMan777 <97909940+ColorMan777@users.noreply.github.com>
2026-01-30 16:10:30 +0100

    more focus things (work better with gamepad)

--- Scripts/S_Main_menu.gd
+++ Scripts/S_Main_menu.gd
@@ -6,11 +6,13 @@ extends Control
 var optionUI
 
 func _ready() -> void:
-	startButton.grab_focus
+	startButton.grab_focus.call_deferred()
 	optionUI = GameSingletons.get_node("MenuLayer")
+	optionUI.unpauseFocusControl = startButton
 
 func _on_start_pressed() -> void:
 	optionUI.visible = false
+	optionUI.unpauseFocusControl = null
 	get_tree().change_scene_to_packed(first_level)
 
 
