f2f73bb4326bb228ee33a224f54536959e239a65
M-Lampin <maxime.lampin.41@gmail.com>
2026-01-30 16:34:34 +0100

    fix bug access_tile_map

--- Scripts/S_access_tile_map.gd
+++ Scripts/S_access_tile_map.gd
@@ -12,9 +12,12 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	SwitchBlockState()
+	pass
 
 func SwitchBlockState():
 	if(AccessibilityBlocks == false):
 		enabled = false
 		#visible = false
+	else:
+		enabled = true
 		
